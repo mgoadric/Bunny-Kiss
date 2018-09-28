93190cd39c9feabe744d6730be9363c9d2cc5786
Mark Goadrich <goadrich@hendrix.edu>
2018-09-28 13:50:48 -0500

    Bunnies are moving!

--- Bunny Kiss/Assets/Scripts/Space.cs
+++ Bunny Kiss/Assets/Scripts/Space.cs
@@ -20,8 +20,24 @@ public class Space : MonoBehaviour {
 		
 	}
 
+    void BunnyCheck(Bunny b, int lookx, int looky)
+    {
+        if (b.x == lookx && b.y == looky)
+        {
+            b.MoveTo(x, y);
+        }
+    }
+
     void OnMouseDown()
     {
         Debug.Log("" + x + "," + y + ":" + value);
+        foreach (Bunny b in challenge.boardBunnies)
+        {
+            // Look in all four directions for any bunnies that can jump.
+            BunnyCheck(b, x + value, y);
+            BunnyCheck(b, x - value, y);
+            BunnyCheck(b, x, y + value);
+            BunnyCheck(b, x, y - value);
+        }
     }
 }
