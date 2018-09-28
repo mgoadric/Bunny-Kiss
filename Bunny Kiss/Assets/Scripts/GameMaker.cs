7dd194de1e344ec6f7d46cc589a67b4f52d19dd3
Mark Goadrich <goadrich@hendrix.edu>
2018-09-28 12:54:44 -0500

    Starting a random setup

--- Bunny Kiss/Assets/Scripts/GameMaker.cs
+++ Bunny Kiss/Assets/Scripts/GameMaker.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class GameMaker : MonoBehaviour {
 
+    private int XSIZE = 5;
+    private int YSIZE = 5;
+    private int[,] values;
+    public GameObject spacefab;
+
 	// Use this for initialization
 	void Start () {
-		
+
+
+        // MAKE A RANDOM LEVEL
+        values = new int[XSIZE, YSIZE];
+
+        for (int i = 0; i < XSIZE; i++)
+        {
+            for (int j = 0; j < YSIZE; j++)
+            {
+                values[i, j] = Random.Range(0, 5);
+            }
+        }
+
+        // MAKE THE OBJECTS BASED ON THE LEVEL
+        for (int i = 0; i < values.GetLength(0); i++)
+        {
+            for (int j = 0; j < values.GetLength(1); j++)
+            {
+                Instantiate(spacefab, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
 	}
 	
 	// Update is called once per frame
